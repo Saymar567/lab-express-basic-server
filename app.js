88ae0c814df12777b51f7f4bc25551ccbe1a2fbd
Mikel Jimenez <majc1907@gmail.com>
2024-05-20 13:35:54 +0200

    done

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
 
+const express = require("express");
+const logger = require("morgan");
 
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
 
-
+const app = express()
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
@@ -14,12 +16,34 @@
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
 
-
+app.use(express.static("public"));
+app.use(express.json("data"));
+app.use(logger("dev"));
 
 // ROUTES
 // Start defining your routes here:
 
+app.get("/", (request, response)=>{
+    console.log("it is working on home");
+    response.sendFile(__dirname + "/views/home.html")
+})
+app.get("/blog", (request, response)=>{
+    console.log("in the blog works too");
+    response.sendFile(__dirname + "/views/blog.html")
+})
+
+app.get("/api/projects",(request, response)=>{
+    response.json(projects)
+})
+app.get("/api/articles", (request, response)=>{
+    response.json(articles)
+})
+
+app.get("/*", (request, response)=>{
+    response.sendFile(__dirname + "/views/not-found.html")
+})
 
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, ()=>console.log("It works on 5005!"))
